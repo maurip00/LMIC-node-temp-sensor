583248c4c1041da2829e224b571db8774708d5fb
maurip00 <42833806+maurip00@users.noreply.github.com>
2022-11-04 23:29:33 +0100

    Update lmic-node-uplink-formatters.js
    In the payload there are the sensor temperature read and the original counter

--- payload-formatters/lmic-node-uplink-formatters.js
+++ payload-formatters/lmic-node-uplink-formatters.js
@@ -5,19 +5,22 @@
  *  Function:     LMIC-node uplink payload formatter JavaScript function(s).
  * 
  *  Author:       Leonel Lopes Parente
+ *
+ *  Mod ver by:   Maurip00
  * 
  *  Description:  These function(s) are for use with The Things Network V3. 
  *                 
  ******************************************************************************/
 
 function decodeUplink(input) {
-    var data = {};
+    var data = {} ;
     var warnings = [];
 
     if (input.fPort == 10) {
-        data.counter = (input.bytes[0] << 8) + input.bytes[1];
+        data.temperature = ((input.bytes[0] << 8) + input.bytes[1])/100;
+        data.counter = (input.bytes[2] << 8) + input.bytes[3];
     }
-    else {
+    else { 
         warnings.push("Unsupported fPort");
     }
     return {
